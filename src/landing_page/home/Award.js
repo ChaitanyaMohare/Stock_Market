a3e26c972c9dde03483944d2f80da4a1673d27a0
Chaitanya Mohare <zmohare@gmail.com>
2026-05-08 10:17:09 +0530

    Updated Stats Section

--- src/landing_page/home/Award.js
+++ src/landing_page/home/Award.js
@@ -4,12 +4,12 @@ function Award() {
     return (  
         <div className="container">
             <div className="row">
-                <div className="col-6 p-5">
+                <div className="col-6 p-5 mb-5">
                     <img src='/images/largestBroker.svg' alt='award'/>
                 </div>
-                <div className="col-6 p-5">
+                <div className="col-6 p-5 mt-5">
                     <h1>Largest Stock Broker in INDIA</h1>
-                    <p>2+ million Zerodha clients contribute to over 15% of all retail order in India this is a huge trade mark of the company:</p>
+                    <p className="mb-5">2+ million Zerodha clients contribute to over 15% of all retail order in India this is a huge trade mark of the company:</p>
                     <div className="row mt-2">
                         <div className="col-6">
                             <ul>
@@ -26,7 +26,7 @@ function Award() {
                             </ul>
                         </div>
                     </div>
-                     <img src='/images/pressLogos.png' alt='News Logos'/>
+                     <img src='/images/pressLogos.png' alt='News Logos' style={{width:"80%"}}/>
                 </div>
             </div>
         </div>
--- src/landing_page/home/Stats.js
+++ src/landing_page/home/Stats.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 function Stats() {
     return (  
-        <h1>Stats</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <div className="Title mb-5 mt-5"><h1>Trust with Confidence</h1></div>
+                    <h3>Costumer-first always</h3>
+                    <p>Thats why 1.3 Cr people trust our website for the buying and selling of the stocks, as we are the No.1 in India called us devil the comeback man</p>
+                    <h3>Costumer-first always</h3>
+                    <p>Thats why 1.3 Cr people trust our website for the buying and selling of the stocks, as we are the No.1 in India called us devil the comeback man</p>
+                    <h3>Costumer-first always</h3>
+                    <p>Thats why 1.3 Cr people trust our website for the buying and selling of the stocks, as we are the No.1 in India called us devil the comeback man</p>
+                    <h3>Costumer-first always</h3>
+                    <p>Thats why 1.3 Cr people trust our website for the buying and selling of the stocks, as we are the No.1 in India called us devil the comeback man</p>
+                </div>
+                <div className="col-6 ">
+                    <img src='/images/ecosystem.png' alt='ecosystem' style={{width:"95%"}}></img>
+                </div>
+            </div>
+        </div>
     );
 }
 
